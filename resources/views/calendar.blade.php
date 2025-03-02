d7672aec83db5e8911a33b9a6eab80c283578174
Kevin PC2 <kevin.kling@outlook.es>
2025-03-02 16:18:20 -0300

    Estilos generales

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -2,7 +2,7 @@
 
 @section('calendar')
 <div class="container mx-auto h-[90vh] flex items-center justify-center p-4">
-    <div id='calendar' class="w-full h-full rounded-xl shadow-lg border "></div>
+    <div id='calendar' class="w-[85%] h-full rounded-xl shadow-lg border "></div>
 </div>
 
 <!-- Modal -->
@@ -10,7 +10,7 @@
     <div class="bg-white rounded-lg shadow-lg w-1/3 p-6 space-y-4">
         <h2 id="modalTitle" class="text-xl font-semibold text-gray-800"></h2>
 
-        <div id="errorContainer" class="text-red-500"></div>
+        <div id="errorContainer" class="text-red-500 text-sm"></div>
 
         <form id="form">
 
@@ -21,36 +21,36 @@
             </div>
 
             <div class="mb-4">
-                <label for="activity" class="block text-gray-700">Tipo de actividad</label>
-                <input type="text" id="activity" name="activity" class="mt-2 p-2 w-full border border-gray-300 rounded-md" required placeholder="Ejemplo: TP, Examen, Exposición..." />
+                <label for="activity" class="block text-gray-700 text-sm">Tipo de actividad</label>
+                <input type="text" id="activity" name="activity" class="mt-2 p-2 w-full border border-gray-300 rounded-md text-sm" required placeholder="Ejemplo: TP, Examen, Exposición..." />
             </div>
 
             <div class="mb-4">
-                <label for="subject" class="block text-gray-700">Materia</label>
-                <input type="text" id="subject" name="subject" class="mt-2 p-2 w-full border border-gray-300 rounded-md" required placeholder="Ejemplo: Programación, Diseño..." />
+                <label for="subject" class="block text-gray-700 text-sm">Materia</label>
+                <input type="text" id="subject" name="subject" class="mt-2 p-2 w-full border border-gray-300 rounded-md text-sm" required placeholder="Ejemplo: Programación, Diseño..." />
             </div>
 
             <div class="mb-4">
-                <label for="start_date" class="block text-gray-700">Fecha de inicio (Hora que se abrió dicha tarea en el campus)</label>
-                <input type="date" id="start_date" name="start_date" class="mt-2 p-2 w-full border border-gray-300 rounded-md" required />
+                <label for="start_date" class="block text-gray-700 text-sm">Fecha de inicio (Hora que se abrió dicha tarea en el campus)</label>
+                <input type="date" id="start_date" name="start_date" class="mt-2 p-2 w-full border border-gray-300 rounded-md text-sm" required />
             </div>
 
             <div class="mb-4">
-                <label for="end_date" class="block text-gray-700">Fecha de fin (Fecha de entrega)</label>
-                <input type="date" id="end_date" name="end_date" class="mt-2 p-2 w-full border border-gray-300 rounded-md" required />
+                <label for="end_date" class="block text-gray-700 text-sm">Fecha de fin (Fecha de entrega)</label>
+                <input type="date" id="end_date" name="end_date" class="mt-2 p-2 w-full border border-gray-300 rounded-md text-sm" required />
             </div>
 
             <div class="mb-4">
-                <label for="notes" class="block text-gray-700">Notas</label>
-                <textarea id="notes" name="notes" class="mt-2 p-2 w-full border border-gray-300 rounded-md" rows="4" placeholder="Ejemplo: Cierra 23:59, Tarea individual..."></textarea>
+                <label for="notes" class="block text-gray-700 text-sm">Notas</label>
+                <textarea id="notes" name="notes" class="mt-2 p-2 w-full border border-gray-300 rounded-md text-sm" rows="4" placeholder="Ejemplo: Cierra 23:59, Tarea individual..."></textarea>
             </div>
 
         </form>
         <div class="flex justify-end space-x-2 mt-4">
-            <button type="submit" style="display: none;" id="btnGuardar" class="px-2 py-2 bg-green-700 text-white rounded-md">Guardar</button>
-            <button type="submit" style="display: none;" id="btnEliminar" class="px-2 py-2 bg-red-500 text-white rounded-md">Eliminar</button>
-            <button type="submit" style="display: none;" id="btnModificar" class="px-2 py-2 bg-yellow-500 text-white rounded-md">Modificar</button>
-            <button type="button" id="closeModalBtn" class="px-2 py-2 bg-gray-300 rounded-md">Cancelar</button>
+            <button type="submit" style="display: none;" id="btnGuardar" class="px-2 py-1.5 bg-green-700 text-white rounded-md text-sm">Guardar</button>
+            <button type="submit" style="display: none;" id="btnEliminar" class="px-2 py-1.5 bg-red-500 text-white rounded-md text-sm">Eliminar</button>
+            <button type="submit" style="display: none;" id="btnModificar" class="px-2 py-1.5 bg-yellow-500 text-white rounded-md text-sm">Modificar</button>
+            <button type="button" id="closeModalBtn" class="px-2 py-1.5 bg-gray-300 rounded-md text-sm">Cancelar</button>
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Kalendar</title>
+    <title>Kalendar | Gestiona tus fechas de exámenes y tareas</title>
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
